00d3824509c68ec909a19614450f61e009963f21
南山まさかず <masakazu.minamiyama@gmail.com>
2013-08-02 12:57:51 +0900

    Completed in provisionally

--- inverse_fizz_buzz.hs
+++ inverse_fizz_buzz.hs
@@ -1,12 +1,14 @@
 {-# OPTIONS -Wall #-}
 
+import Data.List
+
 data FizzBuzz = Fizz | Buzz | FizzBuzz | None deriving ( Read , Eq )
 
 main :: IO ( )
-main = getContents >>= ( return . map read . parseFizzBuzz ) >>= ( return . solve ) >>= ( return . showAnswer ) >>= putStr
+main = getContents >>= ( return . map read . parseFizzBuzz ) >>= ( return . solve ) >>= mapM_ print
 
 parseFizzBuzz :: String -> [ String ]
-parseFizzBuzz = filter ( ` elem ` [ "Fizz" , "Buzz" , "FizzBuzz" ] ) . lines
+parseFizzBuzz = filter ( ` elem ` [ "Fizz" , "Buzz" , "FizzBuzz" ] ) . words
 
 solve :: [ FizzBuzz ] -> [ Int ]
 solve xs = safe_head $ filter isOK $ solveHelper xs
@@ -20,8 +22,12 @@ solveHelper _ = [ ]
 
 safe_head :: [ [ ( FizzBuzz , ( Int , FizzBuzz ) ) ] ] -> [ Int ]
 safe_head [ ] = [ ]
-safe_head xs = case map ( \ ( _ , ( x , _ ) ) -> x ) $ head xs of
-	xs' -> [ head xs' .. last xs' ]
+safe_head xs = head $ sortBy lengthOrd $ map ( \ xs' -> [ head xs' .. last xs' ] ) $ map ( map ( \ ( _ , ( x , _ ) ) -> x ) ) xs
+
+lengthOrd :: ( Ord a ) => [ a ] -> [ a ] -> Ordering
+lengthOrd xs ys
+	| length xs /= length ys = compare ( length xs ) ( length ys )
+	| otherwise = compare xs ys
 
 genFizzBuzz :: Int -> [ ( Int , FizzBuzz ) ]
 genFizzBuzz x
@@ -32,6 +38,3 @@ genFizzBuzz x
 
 isOK :: [ ( FizzBuzz , ( Int , FizzBuzz ) ) ] -> Bool
 isOK = all ( \ ( x1 , ( _ , x2 ) ) -> x1 == x2 )
-
-showAnswer :: [ Int ] -> String
-showAnswer xs = concat $ map ( ++ "\n" ) $ map show xs
